8b396d377385f1de9ac0a756d2d37032bd35dca0
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-02-19 09:30:55 +0100

    updated semantic scholar data to 2019-01-31

--- data/s2/__init__.py
+++ data/s2/__init__.py
@@ -1,11 +1,11 @@
 from data import DATA_HOME, read_gzip_lines
 
 _ENCODING = 'utf-8'
-DATA_FOLDER = DATA_HOME / 's2'
+DATA_FOLDER = DATA_HOME / 's2nex'
 
 
 def file_list():
-    for i in range(40):
+    for i in range(46):
         yield DATA_FOLDER / f's2-corpus-{i:02d}.gz'
 
 
