9a144b4fc3bb3e4c465d9bf6f3945b87f05978c9
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-02-25 19:37:12 +0100

    parallel upload and some comments

--- data/s2/upload.py
+++ data/s2/upload.py
@@ -5,36 +5,111 @@ from multiprocessing import Pool, cpu_count
 import json
 
 
-def parse_json(line):
+"""
+export interface Document {
+    entities:      string[];
+    journalVolume: string; -> volume
+    journalPages:  string; -> pages
+    pmid:          string;
+    year?:         number;
+    outCitations:  string[];
+    s2Url:         string;
+    s2PdfUrl:      string;
+    id:            string;
+    authors:       Author[]; -> author (string[])
+    journalName:   string; -> journal
+    paperAbstract: string;
+    inCitations:   string[];
+    pdfUrls:       string[];
+    title:         string;
+    doi:           string;
+    sources:       Source[];
+    doiUrl:        string;
+    venue:         string;
+}
+
+export interface Author {
+    name: string; -> use only this
+    ids:  string[]; -> discard this
+}
+
+export enum Source {
+    Dblp = "DBLP",
+    Medline = "Medline",
+}
+
+"""
+
+def parse_json(line: str) -> str:
+    """
+    parse one line of json data, change the names of some fields and add the length of multivalued fields
+    :param line: a string containing document as json
+    :return: the modified data as string
+    """
     parsed = json.loads(line)
-    authors = []
+    # rename authors to author and discard ids
     try:
-        authors = [x.get('name','') for x in parsed.pop('authors')]
+        parsed['author'] = [x.get('name', '') for x in parsed.pop('authors')]
     except KeyError:
         pass
-    parsed['author'] = authors
+    # rename journalName to journal
+    try:
+        parsed['journal'] = parsed.pop('journalName')
+    except KeyError:
+        pass
+    try:
+        parsed['pages'] = parsed.pop('journalPages')
+    except KeyError:
+        pass
+    try:
+        parsed['volume'] = parsed.pop('journalVolume')
+    except KeyError:
+        pass
+    try:
+        parsed['keywords'] = parsed.pop('entities')
+    except KeyError:
+        pass
+
     parsed['inCitations_count'] = len(parsed.get('inCitations', []))
     parsed['outCitations_count'] = len(parsed.get('outCitations', []))
-    parsed['entities_count'] = len(parsed.get('entities',[]))
+    parsed['keywords_count'] = len(parsed.get('keywords',[]))
     parsed['author_count'] = len(parsed.get('author',[]))
     return json.dumps(parsed)
 
 
 def batch_jsonl(generator, batchsize):
+    """
+    creates larger chunks from a genenerator function.
+
+    :param generator: the generator function that yields lines of json
+    :param batchsize: the maximum size of the batch
+    :return: yields utf-8 encoded bytes
+    """
     with Pool(processes=cpu_count()) as pool:
         while True:
             batch = itertools.islice(generator, batchsize)
             batch = '\n'.join(pool.imap(parse_json, batch))
             if 0 < len(batch):
-                yield batch
+                yield batch.encode('utf-8')
             else:
                 break
 
 
+def upload_parallel(generator, session):
+    with Pool(processes=1) as pool:
+        yield from pool.imap(session.collection('s2').update.jsonl, generator)
+
+
 if __name__ == '__main__':
+    document_count = 219709 + 39*1_000_000
+    batch_size = 10_000
+    batch_count, rest = divmod(document_count, batch_size)
+    if rest>0:
+        batch_count += 1
+
     s = get_localhost_session()
 
-    reset = False
+    reset = True
     if reset is True:
         print('deleting collection')
         print(s.admin.collections.delete('s2').json())
@@ -50,7 +125,14 @@ if __name__ == '__main__':
 
     print('sending documents')
     counter = 0
-    for batch in batch_jsonl(read_all(),batch_size):  # 3922 batches with 10_000 size
+    batch_generator = batch_jsonl(read_all(),batch_size)
+    for response in upload_parallel(batch_generator, s):  # 3922 batches with 10_000 size
         counter += 1
-        r = s.collection('s2').update.jsonl(batch.encode('utf-8'),commit=True)
-        print(f'finished batch {counter:4d}: {r.json()}')
+        print(f'finished batch {counter:4d}: {response.json()}')
+    # for batch in batch_generator:
+    #     response = s.collection('s2').update.jsonl(batch)
+    #     counter += 1
+    #     print(f'finished batch {counter:4d}: {response.json()}')
+    print('sending commit')
+    r = s.collection('s2').update.xml('<commit/>')
+    print(r.text)
