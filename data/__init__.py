69146a25e0d0de0b7e8014d000683ce1c5564280
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-03-17 14:46:38 +0100

    more generic functions to unclutter upload scripts

--- data/__init__.py
+++ data/__init__.py
@@ -2,9 +2,65 @@ import gzip
 import json
 from json.decoder import JSONDecodeError
 from pathlib import Path
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
+import itertools
+from typing import Callable
 
-DATA_HOME = Path.home() / 'thesis' / 'data'
+DATA_HOME = Path('/storage') / 'thesis' / 'data'
+
+
+def batch_jsonl_parsed(generator, batchsize, parser_fn: Callable[[str], str]):
+    """
+    creates larger chunks from a genenerator function.
+
+    :param generator: the generator function that yields lines of json
+    :param batchsize: the maximum size of the batch
+    :param parser_fn: a preprocessor for the json values
+    :return: yields utf-8 encoded bytes
+    """
+    with Pool(processes=cpu_count()) as pool:
+        while True:
+            batch = itertools.islice(generator, batchsize)
+            batch = '\n'.join(pool.imap(parser_fn, batch))
+            if 0 < len(batch):
+                yield batch.encode('utf-8')
+            else:
+                break
+
+
+def batch_jsonl_unparsed(generator, batchsize=10_000):
+    """
+    creates larger chunks from a genenerator function.
+
+    :param generator: the generator function that yields lines of json
+    :param batchsize: the maximum size of the batch
+    :return: yields utf-8 encoded bytes
+    """
+    while True:
+        batch = itertools.islice(generator, batchsize)
+        batch = '\n,'.join(batch)
+        if 0 < len(batch):
+            yield batch.encode('utf-8')
+        else:
+            break
+
+
+def upload_batches_unparsed(session, collection: str, generator):
+    print('sending documents')
+    counter = 0
+    batch_generator = batch_jsonl_unparsed(generator)
+    for response in upload_parallel(batch_generator, session, collection):  # 3922 batches with 10_000 size
+        counter += 1
+        d = response.json()
+        if d['responseHeader']['status'] != 0:
+            print(f'{d}')
+        print(f'{counter:4d}', end=' ')
+        if counter % 10 == 0:
+            print()
+    print('sending commit')
+    r = session.collection(collection).update.xml('<commit/>')
+    print(r.text)
+
 
 def read_gzip_lines(file, encoding):
     with gzip.open(file, 'rt', encoding=encoding) as f:
@@ -22,3 +78,10 @@ def parse_json(line):
 def parse_jsonl_parallel(generator, processes=4):
     with Pool(processes=processes) as pool:
         yield from pool.imap(parse_json, generator)
+
+
+def upload_parallel(generator, session, collection):
+    with Pool(processes=cpu_count()) as pool:
+        yield from pool.imap(session.collection(collection).update.jsonl, generator)
+
+
