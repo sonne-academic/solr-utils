c8257e1963809e056f1ebe266b5d45f0a3ca770f
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-07-11 17:43:50 +0200

    add get_config and update mag schema

--- solr/configsets/__init__.py
+++ solr/configsets/__init__.py
@@ -2,3 +2,11 @@
 upload configset with:
 (cd server/solr/configsets/sample_techproducts_configs/conf && zip -r - *) | curl -X POST --header "Content-Type:application/octet-stream" --data-binary @- "http://localhost:8983/solr/admin/configs?action=UPLOAD&name=myConfigSet"
 """
+from pathlib import Path
+
+module_path = Path(__file__).parent
+cfg_path = module_path / 'configs'
+
+
+def get_config(name: str):
+    return cfg_path / name
