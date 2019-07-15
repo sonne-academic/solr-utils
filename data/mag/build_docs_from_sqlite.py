ecfdb23742da473242704c037a47f280f4218fc9
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-07-15 15:15:43 +0200

    still slow, try with defaultdict

--- data/mag/build_docs_from_sqlite.py
+++ data/mag/build_docs_from_sqlite.py
@@ -3,6 +3,7 @@ import json
 from datetime import datetime
 import gzip
 from data import DATA_HOME, ItemsPerSecondBar
+import collections
 
 DATA_FOLDER = DATA_HOME / 'mag'
 conn = sqlite3.connect(DATA_FOLDER / 'mag.sqlite3', check_same_thread=False)
@@ -119,45 +120,33 @@ def generate_references_updates():
     ips.finish()
 
 
-def generate_cby_updates():
-    c = conn.cursor()
-    query = 'select PaperReferenceId, PaperId from PaperReferences order by PaperReferenceId'
-    prev_pid = None
-    refs = []
-    ips = ItemsPerSecondBar('references', max=row_count('PaperReferences'))
-    for paperid, ref in c.execute(query):
-        if prev_pid is None:
-            prev_pid = paperid
-        elif prev_pid != paperid:
-            yield {'id': prev_pid, 'cited_by': {'set': refs}, 'cited_by_count': {'set': len(refs)}}
-            refs = []
-            prev_pid = paperid
-        refs.append(ref)
-        ips.next()
-    ips.finish()
-
-
 def generate_cited_by_updates():
+    dd = collections.defaultdict(list)
     c = conn.cursor()
     query = 'select PaperReferenceId, PaperId from PaperReferences order by PaperReferenceId'
-    cites = []
+    # cites = []
     prev_pid = None
     ips = ItemsPerSecondBar('citations', max=row_count('PaperReferences'))
     for paperid, citation in c.execute(query):
-        if prev_pid is None:
-            prev_pid = paperid
-        elif prev_pid != paperid:
-            yield {'id': prev_pid, 'cited_by': {'set': cites}, 'cited_by_count': {'set': len(cites)}}
-            cites = []
-            prev_pid = paperid
-        cites.append(citation)
+        # if prev_pid is None:
+        #     prev_pid = paperid
+        # elif prev_pid != paperid:
+        #     yield {'id': prev_pid, 'cited_by': {'set': cites}, 'cited_by_count': {'set': len(cites)}}
+        #     cites = []
+        #     prev_pid = paperid
+        if len(dd) == 10_000 and paperid not in dd:
+            for k, v in dd.items():
+                yield {'id': k, 'cited_by': {'set': v}, 'cited_by_count': {'set': len(v)}}
+            dd = collections.defaultdict(list)
+        dd[paperid].append(citation)
+        # cites.append(citation)
         ips.next()
     ips.finish()
 
 
 UPDATE_GENERATORS = [
     generate_author_affiliation_updates,
-    generate_cby_updates,
+    generate_cited_by_updates,
     generate_journal_updates,
     generate_references_updates,
     generate_url_updates,
